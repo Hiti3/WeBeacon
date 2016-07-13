e1f84b167e38feff69539019e11db99fd1ca67b6
maticrepse <matic.repse@gmail.com>
2016-07-13 09:42:21 +0200

    Commit od Delopsta.

--- server.js
+++ server.js
@@ -1,43 +1,75 @@
-//za Cloud9...
-if (!process.env.PORT)
+if (!process.env.PORT) {
   process.env.PORT = 8080;
+}
 
-//priprava podatkovne baze
-//var sqlite3 = require('sqlite3').verbose();
-//var pb = new sqlite3.Database('imeBaze.sl3');
+//set up server
+var express =require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-
-// Priprava strežnika
+//moduls
+var path = require('path');
 var formidable = require("formidable");
-var express = require('express');
 var expressSession = require('express-session');
 var fs = require("fs");
-var http = require("html");
-var streznik = express();
-streznik.set('view engine', 'ejs');
-streznik.use(express.static('public'));
-streznik.use(
+var sqlite3 = require('sqlite3').verbose();
+
+//database
+var pb = new sqlite3.Database('database.sl3');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(
   expressSession({
     secret: '1234567890QWERTY', 
     saveUninitialized: true,    
     resave: false,              
     cookie: {
-      maxAge: 3600000       
+    maxAge: 3600000       
     }
   })
 );
 
-//za strezenje staticnih datotek
-streznik.use(express.static("/public"));
-//nastavi path
-var path = require('path');
+/* ------------------------------------------------------------------------- express part ------------------------------------------ */
+app.post('/prijavi', function(zahteva, odgovor) {
+    var form = new formidable.IncomingForm();
+    form.parse(zahteva, function(napaka1, polje, datoteke){
+      zahteva.session.email = polje.email;
+      zahteva.session.geslo = polje.pwd;
+      odgovor.redirect('/tasks');
+    })
+});
 
-//prikazi index.html
-streznik.get('/', function(zahteva, odgovor) {
-    console.log("ping..");
-    odgovor.sendFile(path.join(__dirname, "views/index.html"));
+app.get('/tasks', function(zahteva, odgovor) {
+  pb.all("SELECT * FROM usluzbenci WHERE email = '" +zahteva.session.email + "' AND geslo = '"+zahteva.session.geslo+"'", function(napaka, vrstice){
+    if(vrstice.length == 0){
+      odgovor.redirect('/index');
+    }
+    else{
+      odgovor.render('tasks', {trenutneStranke: vrstice[0]});
+    }
+  })
+});
+
+app.get('/', function(zahteva, odgovor) {
+    odgovor.redirect('/index');
 })
 
-streznik.listen(process.env.PORT, function() {
-    console.log("Strežnik pognan!");
+app.get('/index', function(zahteva, odgovor) {
+    odgovor.render('index', {sporocilo: ' '});
 })
+
+server.listen(process.env.PORT, function() {
+  console.log("Strežnik posluša na portu " + process.env.PORT + ".");
+});
+
+//brodcast to all users
+io.on('connection', function (socket) {
+  socket.on('chat message', function (data) {
+    io.sockets.emit('chat message',data);
+  });
+});
+
+
+
